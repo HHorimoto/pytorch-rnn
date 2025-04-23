36c125c5b82e22915ef692faa6fd1a6e5c778ee1
HHorimoto <81685394+HHorimoto@users.noreply.github.com>
2025-04-23 21:24:08 +0900

    add: model

---
+++ src/models/models.py
@@ -0,0 +1,32 @@
+import torch
+from torch import nn
+import torch.nn.functional as F
+
+class Net(nn.Module):
+    def __init__(self, n_hidden, rnn_name):
+        super(Net, self).__init__()
+
+        self.rnn_name = rnn_name
+
+        if self.rnn_name == "RNN":
+            self.rnn = nn.RNNCell(34, n_hidden)
+        elif rnn_name == "LSTM":
+          self.rnn = nn.LSTMCell(34, n_hidden)
+        elif rnn_name == "GRU":
+          self.rnn = nn.GRUCell(34, n_hidden)
+        
+        self.linear = nn.Linear(n_hidden, 1)
+
+    def forward(self, x, hx, cx):
+        if self.rnn_name == "RNN":
+            hx = self.rnn(x, hx)
+            h = self.linear(hx)
+            return h, hx, cx
+        elif self.rnn_name == "LSTM":
+            hx, cx = self.rnn(x, (hx, cx))
+            h = self.linear(hx)
+            return h, hx, cx
+        elif self.rnn_name == "GRU":
+            hx = self.rnn(x, hx)
+            h = self.linear(hx)
+            return h, hx, cx
