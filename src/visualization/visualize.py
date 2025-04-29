e0dacfad95f9f171e2ee2df5579f531aba3bd1ad
HHorimoto <81685394+HHorimoto@users.noreply.github.com>
2025-04-29 11:14:37 +0900

    add: plot

---
+++ src/visualization/visualize.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+
+def plot(results: dict, metric: str):
+    plt.figure()
+    plt.xlabel('epoch')
+    plt.ylabel(metric)
+    for key, value in results.items():
+        plt.plot(value, label=key)
+    plt.legend()
+    plt.savefig(metric+'.png')
